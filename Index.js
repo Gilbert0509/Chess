6fff2c2bb3cefde32eb62f99c03c6ac37ada5c69
Gilbert0509 <joshua.evason@millburnacademy.org.uk>
2024-03-13 11:14:06 +0000

    Hello

---
+++ Index.js
@@ -0,0 +1,118 @@
+let prompt = require("prompt-sync")();
+console.log("");
+
+//Github initalised 
+
+//column a
+let a1 = "";
+let a2 = "";
+let a3 = "";
+let a4 = "";
+let a5 = "";
+let a6 = "";
+let a7 = "";
+let a8 = "";
+//column b
+let b1 = "";
+let b2 = "";
+let b3 = "";
+let b4 = "";
+let b5 = "";
+let b6 = "";
+let b7 = "";
+let b8 = "";
+
+//comumn c
+let c1 = "";
+let c2 = "";
+let c3 = "";
+let c4 = "";
+let c5 = "";
+let c6 = "";
+let c7 = "";
+let c8 = "";
+
+//column d
+let d1 = "";
+let d2 = "";
+let d3 = "";
+let d4 = "";
+let d5 = "";
+let d6 = "";
+let d7 = "";
+let d8 = "";
+
+//comumn e
+let e1 = "";
+let e2 = "";
+let e3 = "";
+let e4 = "";
+let e5 = "";
+let e6 = "";
+let e7 = "";
+let e8 = "";
+
+//column f
+let f1 = "";
+let f2 = "";
+let f3 = "";
+let f4 = "";
+let f5 = "";
+let f6 = "";
+let f7 = "";
+let f8 = "";
+
+//column g
+let g1 = "";
+let g2 = "";
+let g3 = "";
+let g4 = "";
+let g5 = "";
+let g6 = "";
+let g7 = "";
+let g8 = "";
+
+//column h
+let h1 = "";
+let h2 = "";
+let h3 = "";
+let h4 = "";
+let h5 = "";
+let h6 = "";
+let h7 = "";
+let h8 = "";
+
+console.log("|♖ |♘ |♗ |♕ |♔ |♗ |♘ |♖ |");
+console.log("|♙ |♙ |♙ |♙ |♙ |♙ |♙ |♙ |");
+console.log("|■ |□ |■ |□ |■ |□ |■ |□ |");
+console.log("|□ |■ |□ |■ |□ |■ |□ |■ |");
+console.log("|■ |□ |■ |□ |■ |□ |■ |□ |");
+console.log("|□ |■ |□ |■ |□ |■ |□ |■ |");
+console.log("|♟ |♟ |♟ |♟ |♟ |♟ |♟ |♟ |");
+console.log("|♜ |♞ |♝ |♛ |♚ |♞ |♝ |♜ |");
+
+//|■ |□ |■ |□ |■ |□ |■ |□ |
+//|□ |■ |□ |■ |□ |■ |□ |■ |
+//|■ |□ |■ |□ |■ |□ |■ |□ |
+//|□ |■ |□ |■ |□ |■ |□ |■ |
+//|■ |□ |■ |□ |■ |□ |■ |□ |
+//|□ |■ |□ |■ |□ |■ |□ |■ |
+//|■ |□ |■ |□ |■ |□ |■ |□ |
+//|□ |■ |□ |■ |□ |■ |□ |■ |
+
+function chessLayer(input) {
+  switch (input) {
+  }
+}
+
+function display() {
+  let rowone;
+  let rowtwo;
+  let rowthree;
+  let rowfour;
+
+  chessLayer(rowone);
+  chessLayer(rowtwo);
+  chessLayer(rowthree);
+  chessLayer(rowfour);
+}
