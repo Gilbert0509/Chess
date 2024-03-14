ae06e70e80d302f190647741eb2e0204b995463f
Gilbert0509 <joshua.evason@millburnacademy.org.uk>
2024-03-14 14:50:11 +0000

    Co-authored-by: MudieDj <MudieDj@users.noreply.github.com>

--- Index.js
+++ Index.js
@@ -3,8 +3,42 @@ console.log("");
 
 //Github initalised 
 
+//ChessBoard
+
+const squareBlack = "□"
+
+const squareWhite = "■"
+
+//Black Pieces
+
+const pawnBlack = "♙"
+
+const knightBlack = "♘"
+
+const kingBlack = "♔"
+
+const queenBlack = "♕"
+
+const rookBlack = "♖"
+
+const bishopBlack = "♗"
+
+//White Pieces
+
+const bishopWhite = "♝"
+
+const rookWhite = "♜"
+
+const queenWhite = "♛"
+
+const knightWhite = "♞"
+
+const pawnWhite = "♟"
+
+const kingWhite = "♚"
+
 //column a
-let a1 = "";
+let a1 = blackRook;
 let a2 = "";
 let a3 = "";
 let a4 = "";
@@ -12,6 +46,7 @@ let a5 = "";
 let a6 = "";
 let a7 = "";
 let a8 = "";
+
 //column b
 let b1 = "";
 let b2 = "";
@@ -82,37 +117,171 @@ let h6 = "";
 let h7 = "";
 let h8 = "";
 
-console.log("|♖ |♘ |♗ |♕ |♔ |♗ |♘ |♖ |");
-console.log("|♙ |♙ |♙ |♙ |♙ |♙ |♙ |♙ |");
-console.log("|■ |□ |■ |□ |■ |□ |■ |□ |");
-console.log("|□ |■ |□ |■ |□ |■ |□ |■ |");
-console.log("|■ |□ |■ |□ |■ |□ |■ |□ |");
-console.log("|□ |■ |□ |■ |□ |■ |□ |■ |");
-console.log("|♟ |♟ |♟ |♟ |♟ |♟ |♟ |♟ |");
-console.log("|♜ |♞ |♝ |♛ |♚ |♞ |♝ |♜ |");
 
-//|■ |□ |■ |□ |■ |□ |■ |□ |
-//|□ |■ |□ |■ |□ |■ |□ |■ |
-//|■ |□ |■ |□ |■ |□ |■ |□ |
-//|□ |■ |□ |■ |□ |■ |□ |■ |
-//|■ |□ |■ |□ |■ |□ |■ |□ |
-//|□ |■ |□ |■ |□ |■ |□ |■ |
-//|■ |□ |■ |□ |■ |□ |■ |□ |
-//|□ |■ |□ |■ |□ |■ |□ |■ |
-
-function chessLayer(input) {
-  switch (input) {
-  }
+let moveFromL;
+let moveFromN;
+let moveFromP;
+let movetoL;
+let letmoveN;
+function input(input) {
+  moveFromL = prompt("What was the letter the piece is on")
+  moveFromN = prompt("What was the number the piece is on")
+  moveFromP = prompt("What is the piece")
+  movetoL = prompt("What letter are you moving it to?")
+  movetoN = prompt("What number are you moving it to?")
 }
 
 function display() {
-  let rowone;
-  let rowtwo;
-  let rowthree;
-  let rowfour;
-
-  chessLayer(rowone);
-  chessLayer(rowtwo);
-  chessLayer(rowthree);
-  chessLayer(rowfour);
+  console.log("|" + a1 + "|" + b1 + "|" + c1 + "|" + d1 + "|" + e1 + "|" + f1 + "|" + g1 + "|" + h1 + "|")
+  console.log("|" + a2 + "|" + b2 + "|" + c2 + "|" + d2 + "|" + e2 + "|" + f2 + "|" + g2 + "|" + h2 + "|") 
+  console.log("|" + a3 + "|" + b3 + "|" + c3 + "|" + d3 + "|" + e3 + "|" + f3 + "|" + g3 + "|" + h3 + "|")
+  console.log("|" + a4 + "|" + b4 + "|" + c4 + "|" + d4 + "|" + e4 + "|" + f4 + "|" + g4 + "|" + h4 + "|")
+  console.log("|" + a5 + "|" + b5 + "|" + c5 + "|" + d5 + "|" + e5 + "|" + f5 + "|" + g5 + "|" + h5 + "|")
+  console.log("|" + a6 + "|" + b6 + "|" + c6 + "|" + d6 + "|" + e6 + "|" + f6 + "|" + g6 + "|" + h6 + "|")
+  console.log("|" + a7 + "|" + b7 + "|" + c7 + "|" + d7 + "|" + e7 + "|" + f7 + "|" + g7 + "|" + h7 + "|")
+  console.log("|" + a8 + "|" + b8 + "|" + c8 + "|" + d8 + "|" + e8 + "|" + f8 + "|" + g8 + "|" + h8 + "|")
 }
+
+let boardArrayRep = [[]
+[0, 1, 2, 3, 4, 5, 6, 7, 8],
+[0, 1, 2, 3, 4, 5, 6, 7, 8],
+[0, 1, 2, 3, 4, 5, 6, 7, 8],
+[0, 1, 2, 3, 4, 5, 6, 7, 8],
+[0, 1, 2, 3, 4, 5, 6, 7, 8],
+[0, 1, 2, 3, 4, 5, 6, 7, 8],
+[0, 1, 2, 3, 4, 5, 6, 7, 8],
+[0, 1, 2, 3, 4, 5, 6, 7, 8],
+]
+
+
+function translateMove(moveFroml, moveFromn) {
+  //letter Translate
+  switch(moveFroml) {
+    case "a":
+    moveFromL = 1
+    break;
+    case "b":
+    moveFromL = 2
+    break;
+    case "c":
+    moveFromL = 3
+    break;
+    case "d":
+    moveFromL = 4
+    break;
+    case "e":
+    moveFromL = 5
+    break;
+    case "f":
+    moveFromL = 6
+    break;
+    case "g":
+    moveFromL = 7
+    break;
+    case "h":
+    moveFromL = 8
+    break;
+    default:
+    console.log("Clunie")
+    break;
+  }
+  //Number translate
+  switch(moveFromn) {
+    case "1":
+    moveFromN = 1
+    break;
+    case "2":
+    moveFromN = 2
+    break;
+    case "3":
+    moveFromN = 3
+    break;
+    case "4":
+    moveFromN = 4
+    break;
+    case "5":
+    moveFromN = 5
+    break;
+    case "6":
+    moveFromN = 6
+    break;
+    case "7":
+    moveFromN = 7
+    break;
+    case "8":
+    moveFromN = 8
+    break;
+    default:
+    console.log("Josh")
+    break;
+  }
+  switch(moveToL) {
+    case "a":
+    moveFromL = 1
+    break;
+    case "b":
+    moveFromL = 2
+    break;
+    case "c":
+    moveFromL = 3
+    break;
+    case "d":
+    moveFromL = 4
+    break;
+    case "e":
+    moveFromL = 5
+    break;
+    case "f":
+    moveFromL = 6
+    break;
+    case "g":
+    moveFromL = 7
+    break;
+    case "h":
+    moveFromL = 8
+    break;
+    default:
+    console.log("Clunie")
+    break;
+  }
+  //Number translate
+  switch(movetoN) {
+    case "1":
+    moveFromN = 1
+    break;
+    case "2":
+    moveFromN = 2
+    break;
+    case "3":
+    moveFromN = 3
+    break;
+    case "4":
+    moveFromN = 4
+    break;
+    case "5":
+    moveFromN = 5
+    break;
+    case "6":
+    moveFromN = 6
+    break;
+    case "7":
+    moveFromN = 7
+    break;
+    case "8":
+    moveFromN = 8
+    break;
+    default:
+    console.log("Josh")
+    break;
+  }
+}
+function pawnMove() {
+  
+}
+
+input()
+translateMove(moveFromL, moveFromN)
+console.log(moveFromL)
+console.log(moveFromN)
+console.log(movetoL)
+console.log(moveToN)
